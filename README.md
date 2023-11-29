c7bcc75224b2548f6f280a9450a5fc607f8fa209
Rana Bepari <irana.bpr9@gmail.com>
2023-11-29 16:53:05 +0600

    Modified Files

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 <h3> Simplified Custom Inventory Management System (IMS) </h3>
 <p> Functionality <br> 
+<ol>Authentication</ol>
+<ol>Search Product</ol>
+<ol>Contact Submit</ol>
+<ol>Single Product Page</ol>
+<br>
 <li>Add New Product</li>
 <li>Update New Product</li>
 <li>Store Data</li>
@@ -10,6 +15,10 @@
 -->Delete or Update Data previous image will be removed.
 <br>
 Live Preview of the website <a href="https://sample3.ranasvc.com">Go!</a> <br>
-Home Page
- <img src="screenshots/screenshotx4.png" height="400px">
+
+    <img src="screenshots/screenshotx.png" height="400px">
+    <img src="screenshots/screenshotx1png" height="400px">
+    <img src="screenshots/screenshotx2.png" height="400px">
+    <img src="screenshots/screenshotx3.png" height="400px">
+    <img src="screenshots/screenshotx4.png" height="400px">
  </p>
--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -63,15 +63,15 @@
                 <p>Justo sed diam ut sed amet duo amet lorem amet stet sea ipsum, sed duo amet et. Est elitr dolor elitr erat sit sit. Dolor diam et erat clita ipsum justo sed.</p>
                 <div class="d-flex flex-column mb-3">
                     <h5 class="font-weight-semi-bold mb-3">Store 1</h5>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Mirpur, Dhaka-1216, Bangladesh</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>contact@ranasvc.com</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt text-primary mr-3"></i>+01521380065 (Whatsapp)</p>
                 </div>
                 <div class="d-flex flex-column">
                     <h5 class="font-weight-semi-bold mb-3">Store 2</h5>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
-                    <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Mirpur, Dhaka-1216, Bangladesh</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>contact@ranasvc.com</p>
+                    <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+01521380065 (Whatsapp)</p>
                 </div>
             </div>
         </div>
